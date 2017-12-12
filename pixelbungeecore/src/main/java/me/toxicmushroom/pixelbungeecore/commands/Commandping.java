ba5e1111673be40a2795b92b6de94a49f42852cd
BuildTools <=>
2017-12-12 07:10:42 +0100

    added colors to the message

--- pixelbungeecore/src/main/java/me/toxicmushroom/pixelbungeecore/commands/Commandping.java
+++ pixelbungeecore/src/main/java/me/toxicmushroom/pixelbungeecore/commands/Commandping.java
@@ -1,10 +1,15 @@
 package me.toxicmushroom.pixelbungeecore.commands;
 
 import me.toxicmushroom.pixelbungeecore.Helper;
+import me.toxicmushroom.pixelbungeecore.PixelCore;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
+
+import java.io.IOException;
+import java.net.InetAddress;
 
 public class Commandping extends Command {
 
@@ -17,6 +22,20 @@ public class Commandping extends Command {
         if (sender instanceof ProxiedPlayer) {
             ProxiedPlayer p = (ProxiedPlayer) sender;
             sender.sendMessage(Helper.Colors(Helper.prefix + " &bPing: &c" + p.getPing()));
+        } else {
+            long currentTime = System.currentTimeMillis();
+            boolean isPinged = false; // 2 seconds
+            try {
+                isPinged = InetAddress.getByName("www.google.com").isReachable(9000);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            currentTime = System.currentTimeMillis() - currentTime;
+            if(isPinged) {
+                PixelCore.getInstance().getLogger().info("pinged successfully in " + ChatColor.AQUA + currentTime + "millisecond");
+            } else {
+                System.out.println("Ping is over " + ChatColor.RED + "9000ms.");
+            }
         }
     }
 }
