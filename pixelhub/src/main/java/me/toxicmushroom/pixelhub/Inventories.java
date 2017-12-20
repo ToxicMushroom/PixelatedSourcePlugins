4be8bde3f6a11e3e53866323e508d0bfa43965d8
Merlijn Verstraete <merlijn.verstraete@gmail.com>
2017-12-20 13:45:37 +0100

    Added a itemcraft to easely create items :d

--- pixelhub/src/main/java/me/toxicmushroom/pixelhub/Inventories.java
+++ pixelhub/src/main/java/me/toxicmushroom/pixelhub/Inventories.java
@@ -12,8 +12,6 @@ import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 
 public class Inventories {
@@ -23,10 +21,9 @@ public class Inventories {
 
     public Inventory home() {
         Inventory home = Bukkit.createInventory(null, 36, Helper.Colors("&3&lHome"));
-        ItemStack particles = new ItemStack(Material.POTION, 1);
+        ItemStack particles = itemCrafter(Material.POTION, 1, "&c&lParticles", "Click here to configure your particles!", 0);
         PotionMeta particlesmeta = (PotionMeta) particles.getItemMeta();
         particlesmeta.addCustomEffect(new PotionEffect(PotionEffectType.BLINDNESS, 21, 69), true);
-        particlesmeta.setDisplayName(Helper.Colors("&c&lParticles"));
         particles.setItemMeta(particlesmeta);
         home.setItem(11, particles);
         return home;
@@ -34,62 +31,29 @@ public class Inventories {
 
     public Inventory particles(Player p) {
         Inventory particles = Bukkit.createInventory(null, 18, Helper.Colors("&c&lParticles"));
-        ItemStack item = new ItemStack(Material.APPLE);
-        ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(ChatColor.RED + "Hearths");
-        item.setItemMeta(meta);
-        particles.setItem(1, item);
-
-        ItemStack item2 = new ItemStack(Material.FLINT_AND_STEEL);
-        ItemMeta meta2 = item2.getItemMeta();
-        meta2.setDisplayName(ChatColor.GOLD + "Fire");
-        item2.setItemMeta(meta2);
-        particles.setItem(3, item2);
-
-        ItemStack item3 = new ItemStack(Material.LAVA_BUCKET);
-        ItemMeta meta3 = item3.getItemMeta();
-        meta3.setDisplayName(ChatColor.DARK_RED + "Lava drips");
-        item3.setItemMeta(meta3);
-        particles.setItem(5, item3);
-
-        ItemStack item4 = new ItemStack(Material.WATER_BUCKET);
-        ItemMeta meta4 = item4.getItemMeta();
-        meta4.setDisplayName(ChatColor.BLUE + "Water drips");
-        item4.setItemMeta(meta4);
-        particles.setItem(7, item4);
-
-        ItemStack item5 = new ItemStack(Material.STICK);
-        ItemMeta meta5 = item5.getItemMeta();
-        meta5.setDisplayName(ChatColor.DARK_PURPLE + "Witch crit");
-        item5.setItemMeta(meta5);
-        particles.setItem(11, item5);
-
-        ItemStack item6 = new ItemStack(Material.MILK_BUCKET);
-        ItemMeta meta6 = item6.getItemMeta();
-        meta6.setDisplayName(ChatColor.WHITE + "Milky way");
-        item6.setItemMeta(meta6);
-        particles.setItem(13, item6);
-
-        ItemStack item7 = new ItemStack(Material.NOTE_BLOCK);
-        ItemMeta meta7 = item7.getItemMeta();
-        meta7.setDisplayName(ChatColor.GOLD + "Music notes");
-        item7.setItemMeta(meta7);
-        particles.setItem(14, item7);
-        if (config.getBoolean("particles." + p.getUniqueId() + ".show")) {
-            ItemStack item70 = new ItemStack(Material.WOOL, 1, (short) 5);
-            ItemMeta meta70 = item70.getItemMeta();
-            meta70.setDisplayName(ChatColor.GREEN + "Particles on!");
-            meta70.setLore(Collections.singletonList("Click here to disable particles."));
-            item70.setItemMeta(meta70);
-            particles.setItem(9, item70);
-        } else {
-            ItemStack item80 = new ItemStack(Material.WOOL, 1, (short) 14);
-            ItemMeta meta80 = item80.getItemMeta();
-            meta80.setDisplayName(ChatColor.DARK_RED + "Particles off!");
-            meta80.setLore(Collections.singletonList("Click here to enable particles."));
-            item80.setItemMeta(meta80);
-            particles.setItem(9, item80);
-        }
+        particles.setItem(1, itemCrafter(Material.APPLE, 1, "&dLove", "Click here to select Hearths!", 0));
+        particles.setItem(3, itemCrafter(Material.FLINT_AND_STEEL, 1, "&cFire", "Click here to select Fire!", 0));
+        particles.setItem(5, itemCrafter(Material.LAVA_BUCKET, 1, "&4Baklava", "Click here to select Lava Drips!", 0));
+        particles.setItem(7, itemCrafter(Material.WATER_BUCKET, 1, "&1Magikarp use splash", "Click here to select Water Drips!", 0));
+        particles.setItem(11, itemCrafter(Material.STICK, 1, "&5Witch Crits", "Click here to select Witch Crits!", 0));
+        particles.setItem(13, itemCrafter(Material.MILK_BUCKET, 1, "&fMilky Way", "Click here to select Clouds!", 0));
+        particles.setItem(14, itemCrafter(Material.NOTE_BLOCK, 1, "&aMusic Notes", "Click here to select Music Notes!", 0));
+        particles.setItem(15, itemCrafter(Material.DRAGON_EGG, 1, "&5Dragon Breath", "Click here to select Dragon Breath!", 0));
+        if (config.getBoolean("particles." + p.getUniqueId() + ".show"))
+            particles.setItem(9, itemCrafter(Material.WOOL, 1, "&aParticles enabled!", "Click here to disable particles.", 5));
+        else
+            particles.setItem(9, itemCrafter(Material.WOOL, 1, "&4Particles disabled!", "Click here to enable particles.", 14));
         return particles;
+    }
+
+    private ItemStack itemCrafter(Material material, int amount, String displayname, String lore, int data) {
+        ItemStack toreturn = data == 0 ? new ItemStack(material, amount) : new ItemStack(material, amount, (short) data);
+        ItemMeta metareturn = toreturn.getItemMeta();
+        if (displayname != null)
+            metareturn.setDisplayName(displayname);
+        if (lore != null)
+            metareturn.setLore(Collections.singletonList(lore));
+        toreturn.setItemMeta(metareturn);
+        return toreturn;
     }
 }
